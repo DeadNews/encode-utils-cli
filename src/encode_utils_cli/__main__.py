79ad0bc46e6fb680b0af5b5f15da17eb9b054504
DeadNews <uhjnnn@gmail.com>
2022-08-03 18:13:21 +0200

    0.0.1 (#6)
    - Update tests

---
+++ src/encode_utils_cli/__main__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+"""
+Entry point for cli, enables execution with `python -m encode_utils_cli`
+"""
+from .cli import cli
+
+if __name__ == "__main__":
+    cli()
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python
 from click.testing import CliRunner
 
-from src.encode_utils_cli.cli import cli
+from src.encode_utils_cli.__main__ import cli
 
 
-def test_click():
+def test_cli():
     runner = CliRunner()
-    assert runner.invoke(cli=cli, args=["--help"]).exit_code == 0
+    assert runner.invoke(cli, ["--help"]).exit_code == 0
+    assert (
+        runner.invoke(cli, ["frames-denum", "16886", "26280", "--denum", "2"]).output
+        == "8443 13140\n"
+    )
+    assert (
+        runner.invoke(cli, ["frames-denum", "16886", "26280", "--denum", "0.5"]).output
+        == "33772 52560\n"
+    )
